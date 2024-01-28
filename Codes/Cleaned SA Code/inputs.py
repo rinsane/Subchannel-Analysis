2930415e7ab3e523d321bc6ad49259750605c550
Rishabh Raj <realrishabh9@gmail.com>
2024-01-28 17:15:53 +0530

    inputs and main modded

--- Codes/Cleaned SA Code/inputs.py
+++ Codes/Cleaned SA Code/inputs.py
@@ -4,17 +4,22 @@ class variables:
 
    ALPHA   = 90 # (ANGLE )
    AXLN    = 5.9436
+   DELTA   = 0.5
    FACK    = 0.5
+   FT      = 0.1 # turbulent factor used to compensate imperfect analogy bw turbulent transport of enthalpy and momentum
+   GAMA    = 0.1
    GC      = 9.81 # g
    NCHANL  = 14 # no of subchannel
    NK      = 19 # no of connections
    NNODE   = 97
+   RDIA    = 0.01308  
    RHO     = 817.4  
    SLP     = 0.5                                
+   THETA   = 0.5 # IMPLICIT FAV
    VISC    = 0.000011
 
    def __init__(self):
-      
+
       self.A       = [.000014451,.000014451,.000043225,.000028903,.000043225,.000043225,.000038516000,.000048387,.000038516,.000043225,.000035806,.000071612,.000071612,.000035806]
       
       self.B       = np.zeros(variables.NCHANL)
@@ -29,21 +34,16 @@ class variables:
       
       self.D       = np.zeros(variables.NK)
       self.DELH    = np.zeros(variables.NK)
-      self.DELTA   = 0.5
       self.DELX    = variables.AXLN/(variables.NNODE-1)
       self.DELX    = variables.AXLN/(variables.NNODE-1)
       
       self.ERR     = np.zeros(variables.NK)
-      self.ERR     = np.zeros(variables.NK)
-      self.ERROR   = np.zeros(variables.NK)
       self.ERROR   = np.zeros(variables.NK)
       
       self.F0      = [.090576301,.090576301,.331301059,.1810780788,.331301059,.331301059,.27866318100,.39238281,.278663181,.331301059,.250849063,.501698126,.501698126,.25084906] 
       self.F1      = [0] * 14  
       self.F11     = np.zeros(variables.NK)
-      self.FT      = 0.1 # turbulent factor used to compensate imperfect analogy bw turbulent transport of enthalpy and momentum
       
-      self.GAMA    = 0.1
       self.GAP     = [.0018034,.0009,.0009,.0018034,.0018034,.0009,.0018034,.0009,.0018034,.004140000,.00414,.0018034,.000991,.0019558,.0019558,.000991,.00194,.00194,.00194]  
       
       self.H0      = [1100.7,1100.7,1100.7,1100.7,1100.7,1100.7,1100.7,1100.7,1100.7,1100.7,1100.7000,1100.7,1100.7,1100.7,1100.7,1100.7,1100.7,1100.7000,1100.7,1100.7,1100.7]
@@ -55,9 +55,7 @@ class variables:
       
       self.IC      = [1, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8, 9, 6, 7, 9, 10, 11, 12, 13] # representation of interconnections of different subchannels
       self.JC      = [2, 3, 5, 4, 5, 6, 8, 10, 7, 8, 9, 10, 11, 12, 13, 14, 12, 13, 14]
-      
-      self.NODE    = 1
-      
+            
       self.P0      = np.zeros(variables.NK)
       self.P1      = np.zeros(variables.NK)
       self.P11     = np.zeros(variables.NK)
@@ -67,7 +65,6 @@ class variables:
       
       self.Q       = np.zeros(variables.NCHANL)
       
-      self.RDIA    = 0.01308  
       self.RE      = [0] * 20 
       
       self.S       = np.zeros((variables.NK, variables.NK))
@@ -77,7 +74,6 @@ class variables:
       self.SS      = np.zeros(variables.NCHANL)
       self.ST      = [[0.0] * 20 for _ in range(20)]  
       
-      self.THETA   = 0.5 # IMPLICIT FAV
       
       self.USTAR0  = [0.0] * variables.NK
       self.USTAR1  = [0.0] * variables.NK
