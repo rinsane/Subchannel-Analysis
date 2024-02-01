35cc56c41ecd3f4684a95c32d1fbddc750cd52a4
2022rme0029 <2022rme0029@iitjammu.ac.in>
2024-02-01 18:58:54 +0530

    cleaning

--- Codes/Cleaned SA Code/MAIN.py
+++ Codes/Cleaned SA Code/MAIN.py
@@ -66,8 +66,7 @@ def main():
                 print(tabulate([[NODE[i].WIJ1[I]] for I in range(19)], headers=['WIJ1'], tablefmt = 'grid'))
                 sys.exit()
         #print("All checks passes for P1 and F1 and WIj1, values are listed below")
-        '''print(tabulate([[NODE[i].P1[I], NODE[i].F1[I]] for I in range(14)], headers=['P1', 'F1'], tablefmt = 'grid'))
-        print(tabulate([[NODE[i].WIJ1[I]] for I in range(19)], headers=['WIJ1'], tablefmt = 'grid'))'''
+        
         for I in range(NODE[i].NCHANL):
             NODE[i].ERROR[I] = abs((NODE[i].F11[I] - NODE[i].F1[I]) / NODE[i].F1[I])
         
